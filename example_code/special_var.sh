60d73c568e20a0a40c84febb8b48b1908d8e98ad
leo <lidongjie2012@hust.edu.cn>
2016-12-12 20:14:35 +0800

    some info

---
+++ example_code/special_var.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+echo "\$0: $0"
+echo "\$1: $1"
+echo "\$#: $#"
+echo "\$*: $*"
+echo "\$@: $@"
+echo "\$?: $?"
+echo "\$$: $$"
+
---
+++ example_code/template_code.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# raise exception on error
+set -e
+# print execute command
+set -x
+
+echo "Hello Shell!"
+echo "Hello Bash!"
+
